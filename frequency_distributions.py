2276f86f8d8f3ed7d2c0aff0940565793cdd3a74
RhemJos <rhemjos@gmail.com>
2025-08-27 00:50:47 -0400

    Updated

--- frequency_distributions.py
+++ frequency_distributions.py
@@ -43,36 +43,3 @@ if __name__ == "__main__":
     Freq.generate_charts()
     Freq_1 = FrequencyDistribution(csv_path, sample_size=100, output_dir="frequency_charts_sample")
     Freq_1.generate_charts()
-
-# data_file = pd.read_csv(csv_path, sep=';', encoding='latin1')
-
-# output_dir = "frequency_charts"
-# os.makedirs(output_dir, exist_ok=True)
-
-# for column in data_file.columns[6:]:
-#     frequency = data_file[column].value_counts(dropna=True)
-#     if len(frequency) == 0 or frequency.sum() == 0:
-#         continue
-
-#     try:
-#         sorted_index = sorted(frequency.index, key=lambda x: float(x))
-#         frequency = frequency.loc[sorted_index]
-#     except Exception:
-#         frequency = frequency.sort_index()
-
-#     plt.figure(figsize=(10, 6))
-#     bars = plt.bar(frequency.index.astype(str), frequency.values, color='green', alpha=0.7)
-#     # frequency.plot(kind='bar', color='green', alpha=0.7)
-#     plt.title(f'Distribución de Frecuencias: {column}')
-#     plt.xlabel("Respuesta")
-#     plt.ylabel("Frecuencia")
-
-#     for bar, value in zip(bars, frequency.values):
-#         plt.text(bar.get_x() + bar.get_width() / 2, bar.get_height(), str(value),
-#                  ha='center', va='bottom', fontsize=8)
-
-#     plt.savefig(os.path.join(output_dir, f"{column}_freq.png"))
-#     print(f"Guardado: Gráfico de {column}")
-#     plt.close()
-
-# print(f"Gráficos guardados en la carpeta '{output_dir}'")
